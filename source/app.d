5c8f80f6aeb9655797bee1926159eb314e1683e7
KonstantIMP <mihedovkos@gmail.com>
2021-07-07 16:37:06 +0300

    Add main UI form loading

--- source/app.d
+++ source/app.d
@@ -1,6 +1,39 @@
-import std.stdio;
+/**
+ * Author: KonstantIMP <mihedovkos@gmail.com>
+ * Date: 7 Jul 2021
+ */
+module app;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+/** Import GtkD lib */
+import gtk.Application, gtk.Builder;
+
+/** Import MainWindow */
+import kimp.window;
+
+/**
+ * The sart point of the app
+ * Params:
+ *   args = Input CLI arguments
+ * Returns: 
+ *   0 if everything is OK 
+ */
+int main(string [] args) {
+	/** Create application instance */
+	Application projectNApp = new Application("org.project.n.kimp", GApplicationFlags.FLAGS_NONE);
+	
+	/** Add the apps' activation */
+	projectNApp.addOnActivate((app) {
+		/** Load UI file */
+		Builder bc = new Builder();
+		bc.addFromFile("./resource/project_n.gtk4.ui");
+
+		/** Create the main window */
+		MainWindow projectNWin = new MainWindow(bc);
+
+		/** Set the windows as used */
+		projectNApp.addWindow(projectNWin);
+	});
+
+	/** Run the app and return the result */
+	return projectNApp.run(args);
 }
---
+++ source/kimp/window.d
@@ -0,0 +1,26 @@
+/**
+ * Author: KonstantIMP <mihedovkos@gmail.com>
+ * Date: 7 Jul 2021
+ */
+module kimp.window;
+
+/** Import GtkD widgets */
+import gtk.Window;
+
+/** Import GtkD tools */
+import gtk.Builder;
+
+/**
+ * Main apps' window
+ */
+class MainWindow : Window {
+    /** 
+     * Create the window and prepare it for work
+     * Params:
+     *   uiBuilder = Builder object for UI getting
+     */
+    public this(ref Builder uiBuilder) {
+        super((cast(Window)uiBuilder.getObject("main_win")).getWindowStruct());
+        show();
+    }
+}
