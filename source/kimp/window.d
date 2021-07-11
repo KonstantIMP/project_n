4d4bab1c7fd0670246936d3a45cdfda35ce8e20e
KonstantIMP <mihedovkos@gmail.com>
2021-07-11 23:00:37 +0300

    Connect signals and test VideoPulse

--- source/kimp/window.d
+++ source/kimp/window.d
@@ -5,7 +5,7 @@
 module kimp.window;
 
 /** Import GtkD widgets */
-import gtk.Window, gtk.Box;
+import gtk.Window, gtk.Box, gtk.EditableIF, gtk.Entry, gtk.SpinButton;
 
 /** Import GtkD tools */
 import gtk.Builder : Builder;
@@ -25,8 +25,10 @@ import kimp.plot, kimp.signal;
     public this (ref Builder uiBuilder) {
         super ((cast(Window)uiBuilder.getObject ("main_win")).getWindowStruct ());
 
-        initPlots(uiBuilder);
+        initPlots (uiBuilder);
+        connectSignals (uiBuilder);
 
+        localBuilder = uiBuilder;
         show ();
     }
 
@@ -42,5 +44,57 @@ import kimp.plot, kimp.signal;
         (cast(Box)uiBuilder.getObject("plot_box")).append(video_plot);
     }
 
+    /** 
+     * Connect signal handlers
+     * Params:
+     *   uiBuilder = Builder for getting objects
+     */
+    private void connectSignals (ref Builder uiBuilder) {
+        (cast(EditableIF)uiBuilder.getObject("bits_en")).addOnChanged(&onBitsChanged);
+
+        (cast(SpinButton)uiBuilder.getObject("info_spin")).addOnValueChanged( (spin) {updatePlots ();});
+        (cast(SpinButton)uiBuilder.getObject("snr_spin")).addOnValueChanged( (spin) {updatePlots ();});
+        (cast(SpinButton)uiBuilder.getObject("freq_spin")).addOnValueChanged( (spin) {updatePlots ();});
+    }
+
+    /** 
+     * Update plot's properties
+     */
+    private void updatePlots () {
+        string bits = (cast(Entry)localBuilder.getObject ("bits_en")).getText ();
+
+        double info = (cast(SpinButton)localBuilder.getObject ("info_spin")).getValue ();
+        double snr = (cast(SpinButton)localBuilder.getObject ("snr_spin")).getValue ();
+        double freq = (cast(SpinButton)localBuilder.getObject ("freq_spin")).getValue ();
+    
+        video_plot.setSignal (new VideoPulse (bits, info), bits.length / info);
+
+        video_plot.drawRequest ();
+    }
+
+    /** 
+     * Allows to enter just '1' and '0' to the bits
+     * Params:
+     *   en = Editable of bits entry
+     */
+    protected void onBitsChanged (EditableIF en) {
+        string inputSym = en.getChars (en.getPosition (), en.getPosition () + 1);
+        
+        if (inputSym.length) {
+            if(inputSym[0] != '0' && inputSym[0] != '1') {
+                string correctOut = en.getChars (0, en.getPosition ()) ~ en.getChars (en.getPosition () + 1, -1);
+
+                en.deleteText (0, -1); int zero = 0;
+                en.insertText (correctOut, cast(int)correctOut.length, zero);
+            }
+        }
+
+        updatePlots ();
+    }
+
+    /** Local builder for getting ui elements */
+    private Builder localBuilder;
+
+    /** Plots for display */
     private Plot video_plot;
 }
