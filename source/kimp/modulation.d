f805339e3808bbc99457565c69b83abf996ff122
KonstantIMP <mihedovkos@gmail.com>
2021-07-15 16:18:15 +0300

    Add supported modulation types enum

---
+++ source/kimp/modulation.d
@@ -0,0 +1,13 @@
+/**
+ * Author: KonstantIMP <mihedovkos@gmail.com>
+ * Date: 11 Jul 2021
+ */
+module kimp.modulation;
+
+/** 
+ * Enumeration with supported modulation types
+ */
+enum ModulationType : ubyte {
+    FREQUENCY = 0,
+    PHASE     = 1
+}
